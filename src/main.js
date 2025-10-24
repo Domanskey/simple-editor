80babd454dab2b9bd28c82d5f6aa19739761681e
domanskey <domadam04@gmail.com>
2025-10-24 14:53:51 +0200

    add save and load scripts

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+
+const saveButton = document.querySelector('.button--save');
+const loadButton = document.querySelector('.button--load');
+
+saveButton.addEventListener('click', () => {
+  const text = document.querySelector('.app__textarea').value;
+  localStorage.setItem('savedText', text);
+});
+
+loadButton.addEventListener('click', () => {
+  const savedText = localStorage.getItem('savedText');
+  if (savedText) {
+    document.querySelector('.app__textarea').value = savedText;
+  }
+});
